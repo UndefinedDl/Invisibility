183116e6f4197a979072e9d49818e03b873f5dba
undefined <dilan@botdesigner.io>
2025-05-15 23:42:44 -0300

    feat: instagram viewer

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import InstagramViewer from "@/components/InstagramViewer";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <InstagramViewer />
+    </>
+  );
+}
